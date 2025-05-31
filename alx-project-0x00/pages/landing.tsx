96a8db4e18e5f9b8144bde5fa1b286b6a73969fb
Esau Jesse <jesseesau13@gmail.com>
2025-05-31 13:58:43 +0100

    Edit landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -12,17 +12,17 @@ const Landing: React.FC = () => {
                 {/* Small Buttons */}
                 <Button title="Small - rounded-sm" styles="bg-blue-400 text-black px-3 py-1 text-sm border rounded-sm" />
                 <Button title="Small - rounded-md" styles="bg-blue-600 px-3 py-1 text-sm rounded-md" />
-                <Button title="Small - rounded-full" styles="bg-blue-600 px-3 py-1 text-sm rounded-full" />
+                <Button title="Small - rounded-lg" styles="bg-blue-600 px-3 py-1 text-sm rounded-full" />
 
                 {/* Medium Buttons */}
                 <Button title="Medium - rounded-sm" styles="bg-green-600 px-4 py-2 text-base rounded-sm" />
                 <Button title="Medium - rounded-md" styles="bg-green-600 px-4 py-2 text-base rounded-md" />
-                <Button title="Medium - rounded-full" styles="bg-green-600 px-4 py-2 text-base rounded-full" />
+                <Button title="Medium - rounded-lg" styles="bg-green-600 px-4 py-2 text-base rounded-full" />
 
                 {/* Large Buttons */}
                 <Button title="Large - rounded-sm" styles="bg-red-600 px-6 py-3 text-lg rounded-sm" />
                 <Button title="Large - rounded-md" styles="bg-red-600 px-6 py-3 text-lg rounded-md" />
-                <Button title="Large - rounded-full" styles="bg-red-600 px-6 py-3 text-lg rounded-full" />
+                <Button title="Large - rounded-lg" styles="bg-red-600 px-6 py-3 text-lg rounded-full" />
 
             </div>
         </div>
